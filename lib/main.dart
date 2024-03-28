38c0cd319ce4c6d9ea68d44ce3ee2e973d59744e
Hurera Bhalli <hurerabhalli@gmail.com>
2024-03-29 01:42:02 +0500

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,24 +13,71 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
        home: Scaffold(
          appBar: AppBar(
-           title: Text('Widget Play')
+           title: Text('Widget Play'),
+           backgroundColor: Colors.green,
          ),
           body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Row(
+                children: [
+
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      color: Colors.green,
+                      height: 150,
+
+                      child: Center(child: Text('Hey, Huerea')),
+                    ),
+                  ),
+
+                  Expanded(
+                    flex: 5,
+                    child: Container(
+                      color: Colors.blue,
+                      height: 150,
+
+                      child: Center(child: Text('Hey, Huerea')),
+                    ),
+                  )
+                ],
+              ),
               Center(
                 child: Container(
-                  height: 200,
+                  height: 250,
                   width: 200,
-                  decoration: const BoxDecoration(
+                  // transform: Matrix4.rotationZ(0.2),
+                  margin: EdgeInsets.only(
+                    left: 40,
+                  ),
+                  decoration: BoxDecoration(
                     color: Colors.orange,
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(40),
-                      // bottomLeft: Radius.circular(40),
-                      // bottomRight: Radius.circular(15),
-                      // topLeft: Radius.circular(15)
-                    )
+
+                      borderRadius: BorderRadius.circular(6),
+
+                    // border: Border.all(
+                    //   color: Colors.red,
+                    //   width: 1.2
+                    // ),
+                  // border: Border.all(
+                    //   color: Colors.red,
+                    //   width: 1.2
+                    // ),
+
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: NetworkImage('https://images.pexels.com/photos/6143996/pexels-photo-6143996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')
+                    ),
+
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 100,
+
+                      )
+                    ]
 
                   ),
                   child: Center(child: Text('Container 1')),
@@ -41,3 +89,33 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+
+//Row widget with defining the expanded
+
+//
+// Row(
+// children: [
+//
+// Expanded(
+// flex: 3,
+// child: Container(
+// color: Colors.green,
+// height: 150,
+//
+// child: Center(child: Text('Hey, Huerea')),
+// ),
+// ),
+//
+// Expanded(
+// flex: 5,
+// child: Container(
+// color: Colors.blue,
+// height: 150,
+//
+// child: Center(child: Text('Hey, Huerea')),
+// ),
+// )
+// ],
+// ),
